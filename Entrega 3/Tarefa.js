56d771992cc7eb4be6f458e69b6ae3e95c124151
gumendess <gmendes14@gmail.com>
2016-03-05 15:48:05 -0300

    Tarefa Observer

---
+++ Entrega 3/Tarefa.js
@@ -0,0 +1,41 @@
+function GerarListener() {
+	
+	var obj = {};
+	var lista = [];
+	var i;
+	var count = 0;
+	
+	obj.addListener = function(listener){
+		lista.push(listener);
+	};
+	
+	obj.executar = function (){
+		count++;
+		for(i=0;i<lista.length;i++){
+			var retorno = lista[i];
+			retorno(count);
+		}
+		
+	};
+	
+	obj.returnCount = function(){
+		return count;
+	}
+	
+	return obj;
+	}
+var contadorObserver = GerarListener();
+var listener = function(count){
+	console.log('Count: '+count);
+};
+contadorObserver.addListener(listener);
+
+contadorObserver.executar();
+contadorObserver.executar();
+
+var contadorObserver2 = GerarListener();
+contadorObserver2.addListener(listener);
+
+contadorObserver2.executar();
+contadorObserver2.executar();
+
